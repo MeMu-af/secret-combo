8dbea6431d20498923a480dd4d040cc307545bc5
Trevor <trevlewmath@gmail.com>
2025-02-18 12:42:04 -0500

    add 2 new files

---
+++ script.js
@@ -0,0 +1,8 @@
+//This is the message to the user
+let message = "You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//These are the mathematical clues to the combination
+let num1 = 4 + 6
+let num2 = 10 * 4
+let num3 = 13 * 3
+//This will generate a pop-up alert
+alert(`the vault code is ${num1}-${num2}-${num3}`)
